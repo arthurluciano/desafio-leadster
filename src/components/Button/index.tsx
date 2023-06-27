abc14b44b464628129c1fc4209f436785b9f1e29
arthurluciano <arthurluciano7@gmail.com>
2023-06-27 00:43:43 -0300

    refactor(button): improve component reusability

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,8 @@
-import { AnchorHTMLAttributes } from 'react'
+import { ComponentProps } from 'react'
 
 import { PrimaryButton } from './styles'
 
-type ButtonProps = AnchorHTMLAttributes<HTMLAnchorElement>
+type ButtonProps = ComponentProps<typeof PrimaryButton>
 
 export function Button(props: ButtonProps) {
   return <PrimaryButton {...props} />
